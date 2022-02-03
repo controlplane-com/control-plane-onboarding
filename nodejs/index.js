da70a90d5b47bf8b846f5abc11772a952629e9ce
Eric K <enk21@outlook.com>
2022-02-03 10:29:26 -0800

    nodejs example

---
+++ nodejs/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', root);
+
+app.listen(8080);
+
+function root(req, res) {
+
+	console.log('Request Received');
+	console.log('Workload Name: ' + process.env.CPLN_WORKLOAD);
+    console.log('Location: ' + process.env.CPLN_LOCATION);
+
+	res.send('Hello!');
+}
