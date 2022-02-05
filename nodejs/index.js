8b6ac3a3b6a87a3c29036d6093545c31245d19ea
Eric K <enk21@outlook.com>
2022-02-04 19:03:43 -0800

    Fixing indenting.

--- nodejs/index.js
+++ nodejs/index.js
@@ -9,7 +9,7 @@ function root(req, res) {
 
 	console.log('Request Received');
 	console.log('Workload Name: ' + process.env.CPLN_WORKLOAD);
-    console.log('Location: ' + process.env.CPLN_LOCATION);
+	console.log('Location: ' + process.env.CPLN_LOCATION);
 
 	res.send('Hello!');
 }
